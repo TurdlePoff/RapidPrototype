3bb29572755d6531e8d0d65395838b9d853f975b
Fruzum <sallygillbanks@gmail.com>
2018-07-18 11:39:14 +1200

    Player part movement

---
+++ RapidPrototype1/Assets/Scripts/Movement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour 
+{
+	public int m_PlayerNumber = 1;
+	public float m_speed = 6f;
+
+	Vector3 movement;
+	Rigidbody playerRigidbody;
+}
