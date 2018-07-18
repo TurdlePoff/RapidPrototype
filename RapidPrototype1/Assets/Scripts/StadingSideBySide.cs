5545e736d0cc6003935de87bbe6ee96bacfab90a
Fruzum <sallygillbanks@gmail.com>
2018-07-18 17:18:02 +1200

    Particles

--- RapidPrototype1/Assets/Scripts/StadingSideBySide.cs
+++ RapidPrototype1/Assets/Scripts/StadingSideBySide.cs
@@ -8,18 +8,32 @@ public class StadingSideBySide : MonoBehaviour
 	public Mana manaScript;
 
 	private int nextUpdate = 1;
+    private GameObject ps;
+    private ParticleSystem em;
 
-	void Start()
-	{
-		manaScript = GetComponentInParent<Mana> ();
+
+    void Start()
+    {
+        ps = GameObject.Find("ps");
+        em = ps.GetComponent<ParticleSystem>();
+       // var emission = em.emission;
+       // emission.enabled = false;
+
+        manaScript = GetComponentInParent<Mana> ();
 		if (null == manaScript) {
 			Debug.Log ("Can't find mana script");
 		}
 	}
 
+    void Update()
+    {
+        //var emission = em.emission;
+        //emission.enabled = false;
+    }
+
 	void OnTriggerStay(Collider other)
 	{
-		if (other.tag == "Player" && (Time.time >= nextUpdate)) 
+        if (other.tag == "Player" && (Time.time >= nextUpdate)) 
 		{
 			if (manaScript != null) {
 				manaScript.GainMana (ManaGainAmount);
@@ -29,6 +43,16 @@ public class StadingSideBySide : MonoBehaviour
 			}
 
 			nextUpdate = Mathf.FloorToInt (Time.time) + 1;
-		}
+
+            //var emission = em.emission;
+           // emission.enabled = true;
+            em.Play();
+        }
 	}
+
+    void OnTriggerExit()
+    {
+        //var emission = em.emission;
+        //emission.enabled = false;
+    }
 }
