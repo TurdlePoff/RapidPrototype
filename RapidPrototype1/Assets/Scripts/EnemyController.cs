ebe93afc93fc9ec50804bc1900dc0ad389e07205
Vivian <vivianngo12@hotmail.com>
2018-07-18 17:53:23 +1200

    EnemyMovement
    Follows one character
    ToDo:
    - allow a random character to be followed

---
+++ RapidPrototype1/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+    public float lookRadius = 10.0f;
+    Transform target;
+    NavMeshAgent agent;
+
+    int playertoTarget = 0;
+
+    // Use this for initialization
+    void Start() {
+
+        //playertoTarget = Random.Range(0, 1);
+        //target[0] = PlayerManager.instance.player1.transform;
+
+        target = PlayerManager.instance.player1.transform;
+        //target[1] = PlayerManager.instance.player2.transform;
+
+
+
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            if (distance <= agent.stoppingDistance)
+            {
+                //attack target
+                //face target
+            }
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5.0f);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
