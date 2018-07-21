5fb4971108f96bd7c89df2fee686e76dba240074
Fruzum <sallygillbanks@gmail.com>
2018-07-21 16:03:44 +1200

    Test

---
+++ RapidPrototype1/Assets/Scripts/ManaPotionScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaPotionScript : MonoBehaviour
+{
+    public float manaIncreaseAmount = 5f;
+
+    private Mana manaScript;
+
+    void Start()
+    {
+        GameObject parentOfPlayers = GameObject.FindGameObjectWithTag("PlayerHolder");
+        manaScript = parentOfPlayers.GetComponent<Mana>();
+        if (null == manaScript)
+        {
+            Debug.Log("Can't find mana script - IncreaseMana");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (manaScript != null)
+            {
+                manaScript.GainMana(manaIncreaseAmount);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
