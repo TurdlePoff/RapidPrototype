890e1c8e5b21ebaa6cff97d571012a8688977ad3
Vivian <vivianngo12@hotmail.com>
2018-07-25 19:50:05 +1200

    UI alterations and start screen addition

--- RapidPrototype1/Assets/MainGameScene/Scripts/MenuScreen.cs
+++ RapidPrototype1/Assets/MainGameScene/Scripts/MenuScreen.cs
@@ -7,22 +7,32 @@ public class MenuScreen : MonoBehaviour
 {
     public GameObject MainMenuUI;
     public GameObject HelpMenuUI;
+    public GameObject StartMenuUI;
 
     public void PlayGame()
     {
         SceneManager.LoadScene("Scene1");
     }
 
+    public void LoadStartMenu()
+    {
+        StartMenuUI.SetActive(true);
+        HelpMenuUI.SetActive(false);
+        MainMenuUI.SetActive(false);
+    }
     public void LoadMainMenu()
     {
         MainMenuUI.SetActive(true);
         HelpMenuUI.SetActive(false);
+        StartMenuUI.SetActive(false);
     }
     public void LoadHelpMenu()
     {
         HelpMenuUI.SetActive(true);
         MainMenuUI.SetActive(false);
+        StartMenuUI.SetActive(false);
     }
+
 
     public void QuitGame()
     {
