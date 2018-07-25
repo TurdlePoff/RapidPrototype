f9bbc93785a9e4238df88894a9bc2d0371b4f34e
Fruzum <sallygillbanks@gmail.com>
2018-07-25 13:59:11 +1200

    Attempting bullets spawn paticles on hit

---
+++ RapidPrototype1/Assets/SpawnExplosion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnExplosion : MonoBehaviour
+{
+    public GameObject ParticleHolder;
+
+    private ParticleSystem em;
+
+    private void Start()
+    {
+        em = ParticleHolder.GetComponentInChildren<ParticleSystem>();
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            em.Play();
+            Debug.Log("Hit");
+        }
+    }
+}
